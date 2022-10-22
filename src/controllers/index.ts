0a72ca64b59f84ee67d461c38e3b6e8cbc01ab64
carta87 <cart_albert@hotmail.com>
2022-10-22 10:04:17 -0500

    mi primer proyecto

---
+++ src/controllers/index.ts
@@ -0,0 +1,6 @@
+export * from './ping.controller';
+export * from './usuario.controller';
+
+export * from './cliente.controller';
+export * from './encomienda.controller';
+export * from './servicios.controller';
