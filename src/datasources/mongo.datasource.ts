595c541860f913928e1f650f2e11793e7e3fb72b
carta87 <cart_albert@hotmail.com>
2022-10-27 21:25:03 -0500

    archivo d configuraciones

--- src/datasources/mongo.datasource.ts
+++ src/datasources/mongo.datasource.ts
@@ -1,10 +1,11 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
+import {configuracion} from '../config/config';
 
 const config = {
   name: 'mongo',
   connector: 'mongodb',
-  url: 'mongodb+srv://Admin:Carta87.@misiontic.sk8qo1h.mongodb.net/test',
+  url: configuracion.cadenaBD,
   host: '',
   port: 0,
   user: '',
