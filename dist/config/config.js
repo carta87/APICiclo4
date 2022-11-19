5f106dc19ca887c0218bd1d65ad0d0dc3d6533a0
carta87 <cart_albert@hotmail.com>
2022-11-19 13:46:46 -0500

    make it better

---
+++ dist/config/config.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.configuracion = void 0;
+var configuracion;
+(function (configuracion) {
+    configuracion.claveJWT = "MisionTIC";
+    configuracion.baseURL = "http://127.0.0.1:5000/";
+    configuracion.cadenaBD = "mongodb+srv://Admin:Carta87.@misiontic.sk8qo1h.mongodb.net/test";
+    //export const cadenaBD = "mongodb+srv://Admin:Carta87.@misiontic.sk8qo1h.mongodb.net/?retryWrites=true&w=majority"
+    configuracion.tipoComunicacion = 'email';
+})(configuracion = exports.configuracion || (exports.configuracion = {}));
+//# sourceMappingURL=config.js.map
---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+export namespace configuracion {
+  export const claveJWT = "MisionTIC";
+  export const baseURL = "http://127.0.0.1:5000/";
+  export const cadenaBD = "mongodb+srv://Admin:Carta87.@misiontic.sk8qo1h.mongodb.net/test";
+  //export const cadenaBD = "mongodb+srv://Admin:Carta87.@misiontic.sk8qo1h.mongodb.net/?retryWrites=true&w=majority"
+  export const tipoComunicacion = 'email';
+}
