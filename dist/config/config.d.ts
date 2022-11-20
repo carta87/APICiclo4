2a1367100ab9f1e47073b3ace3ef03eb198e32dd
carta87 <cart_albert@hotmail.com>
2022-11-19 20:38:49 -0500

    make it better

--- dist/config/config.d.ts
+++ dist/config/config.d.ts
@@ -1,6 +1,6 @@
 export declare namespace configuracion {
     const claveJWT = "MisionTIC";
-    const baseURL = "http://127.0.0.1:5000/";
+    const baseURL = "https://apinotificgrupo65equipo4mision.herokuapp.com/";
     const cadenaBD = "mongodb+srv://Admin:Carta87.@misiontic.sk8qo1h.mongodb.net/test";
     const tipoComunicacion = "email";
 }
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export namespace configuracion {
   export const claveJWT = "MisionTIC";
-  export const baseURL = "http://127.0.0.1:5000/";
+  export const baseURL = "https://apinotificgrupo65equipo4mision.herokuapp.com/";
   export const cadenaBD = "mongodb+srv://Admin:Carta87.@misiontic.sk8qo1h.mongodb.net/test";
   //export const cadenaBD = "mongodb+srv://Admin:Carta87.@misiontic.sk8qo1h.mongodb.net/?retryWrites=true&w=majority"
   export const tipoComunicacion = 'email';
