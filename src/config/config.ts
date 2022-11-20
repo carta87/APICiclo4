6a4fad321f214c36068d65cc80c7449d22ecc0fd
carta87 <cart_albert@hotmail.com>
2022-11-20 08:35:34 -0500

    make it better

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export namespace configuracion {
   export const claveJWT = "MisionTIC";
-  export const baseURL = "https://apinotificgrupo65equipo4mision.herokuapp.com";
+  export const baseURL = "https://apinotificgrupo65equipo4mision.herokuapp.com/";
   export const cadenaBD = "mongodb+srv://Admin:Carta87.@misiontic.sk8qo1h.mongodb.net/test";
   //export const cadenaBD = "mongodb+srv://Admin:Carta87.@misiontic.sk8qo1h.mongodb.net/?retryWrites=true&w=majority"
   export const tipoComunicacion = 'email';
