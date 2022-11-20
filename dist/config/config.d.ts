3782ddf43801c5915133f44d61407b556f2e077a
carta87 <cart_albert@hotmail.com>
2022-11-19 20:45:56 -0500

    make it better

--- dist/config/config.d.ts
+++ dist/config/config.d.ts
@@ -1,6 +1,6 @@
 export declare namespace configuracion {
     const claveJWT = "MisionTIC";
-    const baseURL = "https://apinotificgrupo65equipo4mision.herokuapp.com/";
+    const baseURL = "https://apinotificgrupo65equipo4mision.herokuapp.com";
     const cadenaBD = "mongodb+srv://Admin:Carta87.@misiontic.sk8qo1h.mongodb.net/test";
     const tipoComunicacion = "email";
 }
